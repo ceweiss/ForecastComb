394ff7aac9975448559e5f8fa204e1e8501d058a
Gernot Roetzer <gernot.roetzer@gmail.com>
2016-11-09 18:07:11 +0000

    Changed the summary function into a summary class with printing capabilities

--- R/summary.foreccomb_res.R
+++ R/summary.foreccomb_res.R
@@ -1,20 +1,38 @@
 
-summary.foreccomb_res<-function(x, plot=TRUE){
+summary.foreccomb_res<-function(x){
   if(class(x)!="foreccomb_res") stop("Data must be class 'foreccomb'. See ?foreccomb, to bring data in correct format.", call.=FALSE)
+  
+  ans <- list()
+  
+  ans$Method<-x$Method
+  
+  ans$weight<-matrix(x$Weights, ncol=1)
+  colnames(ans$weight)<-"Combination Weight"
+  rownames(ans$weight)<-x$Models
+  
+  ans$Intercept<-x$Intercept
+  
+  ans$accuracy<-rbind(x$Accuracy_Train[1:5], x$Accuracy_Test)
+  rownames(ans$accuracy)[1]<-"Training Set"
+  
+  ans<-append(ans, subset(x, !(names(x) %in% c("Method", "Weights", "Intercept", "Accuracy_Train", "Accuracy_Test"))))
+  
+  class(ans) <- "foreccomb_res_summary"
+  return(ans)
+}
+
+print.foreccomb_res_summary <- function(x, plot=TRUE) {
+  if(class(x)!="foreccomb_res_summary") stop("Data must be class 'foreccomb_res_summary'", call.=FALSE)
   
   cat("\n")
   cat("Summary of Forecast Combination \n")
   cat("------------------------------- \n")
   cat("\n")
-  Method<-x$Method
-  cat("Method: ", Method, "\n")
+  cat("Method: ", x$Method, "\n")
   cat("\n")
   cat("Individual Forecasts & Combination Weights: \n")
   cat("\n")
-  weight<-matrix(x$Weights, ncol=1)
-  colnames(weight)<-"Combination Weight"
-  rownames(weight)<-x$Models
-  print(weight)
+  print(x$weight)
   cat("\n")
   if (!is.null(x$Intercept)){
     cat("Intercept (Bias-Correction): ", x$Intercept, "\n")
@@ -22,9 +40,7 @@ summary.foreccomb_res<-function(x, plot=TRUE){
   }
   cat("Accuracy of Combined Forecast: \n")
   cat("\n")
-  accuracy<-rbind(x$Accuracy_Train[1:5], x$Accuracy_Test)
-  rownames(accuracy)[1]<-"Training Set"
-  print(accuracy)
+  print(x$accuracy)
   cat("\n")
   cat("Additional information can be extracted from the combination object: \n")
   cat("For fitted values (training set): ", paste0(deparse(substitute(x)), "$Fitted"), "\n")
