ee1fb3e774d6c0d8af78f0d91aec36d898c4db2b
Gernot Roetzer <gernot.roetzer@gmail.com>
2016-11-09 16:30:41 +0000

    Added imputation capabilities for foreccomb

--- R/foreccomb.R
+++ R/foreccomb.R
@@ -1,5 +1,20 @@
-foreccomb <- function (observed_vector, prediction_matrix, newobs=NULL, newpreds=NULL, byrow=FALSE)
+imp.values<-function(prediction_matrix, newpreds) {
+  nobs<-nrow(prediction_matrix)
+  whole_data<-rbind(prediction_matrix, newpreds) 
+  imp_data<-as.matrix(mnimput(~.,as.data.frame(whole_data),eps=1e-3,ts=TRUE, method="spline")$filled.dataset)
+  prediction_matrix<-imp_data[1:nobs,]
+  if(!is.null(newpreds)) {
+    newpreds<-imp_data[(nobs+1):nrow(imp_data),]
+  }
+  return(list(prediction_matrix=prediction_matrix, newpreds=newpreds))
+}
+
+foreccomb <- function (observed_vector, prediction_matrix, newobs=NULL, newpreds=NULL, byrow=FALSE, na.impute=TRUE)
 {
+  pckg<-c("mtsdi")
+  temp<-unlist(lapply(pckg, require, character.only=TRUE))
+  if (!all(temp==1)) stop("This function requires packages \"mtsdi\".\n Use install.packages() if it is not yet installed.\n", call.=FALSE) 
+  
   if(is.null(observed_vector)) stop("Training set must contain vector of actual values.", call.=FALSE)
   if(!is.null(dim(observed_vector)) && sum(dim(observed_vector)>1)>1) stop("The input for 'observed vector' appears to be multidimensional. Training set requires a vector of actual values.", call.=FALSE)
   if(!is.numeric(observed_vector)) stop("Actual observations (Training Set) are not numeric.", call.=FALSE)
@@ -37,10 +52,18 @@ foreccomb <- function (observed_vector, prediction_matrix, newobs=NULL, newpreds
     pred_na[i]<-ifelse(sum(is.na(prediction_matrix[,i]))>0, FALSE, TRUE)
   }
   if (sum(pred_na)!=nmodels){
-    message("A subset of the individual forecasts included NA values and has been removed.")
+    if(na.impute) {
+      imputed_values<-imp.values(prediction_matrix, newpreds)
+      prediction_matrix<-imputed_values$prediction_matrix
+      newpreds<-imputed_values$newpreds
+      pred_na<-rep(TRUE, nmodels)
+      message("A subset of the individual forecasts included NA values and has been imputed.")
+    } else {
+      prediction_matrix<-prediction_matrix[,pred_na]
+      nmodels<-ncol(prediction_matrix)
+      message("A subset of the individual forecasts included NA values and has been removed.")
+    }
   }
-  prediction_matrix<-prediction_matrix[,pred_na]
-  nmodels<-ncol(prediction_matrix)
   
   if (nmodels <= 1L){
     stop("Forecast combination methods require individual forecasts from at least 2 models.", call.=FALSE)
@@ -69,7 +92,13 @@ foreccomb <- function (observed_vector, prediction_matrix, newobs=NULL, newpreds
         pred_na_test[i]<-ifelse(sum(is.na(newpreds[,i]))>0, FALSE, TRUE)
       }
       if (sum(pred_na_test)!=nmodels){
-        stop("Prediction matrix (Test Set) must not contain missing values.", call.=FALSE)
+        if(na.impute) {
+          imputed_values<-imp.values(prediction_matrix, newpreds)
+          newpreds<-imputed_values$newpreds
+          message("A subset of the individual forecasts included NA values and has been imputed.")
+        } else {
+          stop("Prediction matrix (Test Set) must not contain missing values.", call.=FALSE)
+        }
       }
       if (is.null(newobs)){
         output <- structure(list(Actual_Train = observed_vector, Forecasts_Train = prediction_matrix, Forecasts_Test = newpreds, nmodels = nmodels, modelnames = modelnames),
