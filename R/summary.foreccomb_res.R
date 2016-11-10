ee90f97418b1b557ead37d820091adab350f71ff
Gernot Roetzer <gernot.roetzer@gmail.com>
2016-11-10 01:24:32 +0000

    Fixed the problem with the plot function

--- R/summary.foreccomb_res.R
+++ R/summary.foreccomb_res.R
@@ -1,4 +1,3 @@
-
 summary.foreccomb_res<-function(x, plot=TRUE){
   if(class(x)!="foreccomb_res") stop("Data must be class 'foreccomb'. See ?foreccomb, to bring data in correct format.", call.=FALSE)
   
@@ -13,19 +12,24 @@ summary.foreccomb_res<-function(x, plot=TRUE){
   } else {
     ans$weight<-"Weights of the individual forecasts differ over time with trimmed mean"
   }
-
+  
   ans$Intercept<-x$Intercept
   
   ans$accuracy<-rbind(x$Accuracy_Train[1:5], x$Accuracy_Test)
   rownames(ans$accuracy)[1]<-"Training Set"
   
+  ans$data<-deparse(substitute(x))
+  
   ans<-append(ans, subset(x, !(names(x) %in% c("Method", "Weights", "Intercept", "Accuracy_Train", "Accuracy_Test"))))
   
+  class(ans) <- c("foreccomb_res_summary")
+  
   if(plot==TRUE){
-    plot(x)
+    p<-plot(ans)
   }
   
-  class(ans) <- c("foreccomb_res_summary")
+  ans$plot<-p
+  
   return(ans)
 }
 
@@ -56,14 +60,14 @@ print.foreccomb_res_summary <- function(x, plot=TRUE) {
   print(x$accuracy)
   cat("\n")
   cat("Additional information can be extracted from the combination object: \n")
-  cat("For fitted values (training set): ", paste0(deparse(substitute(x)), "$Fitted"), "\n")
+  cat("For fitted values (training set): ", paste0(x$data, "$Fitted"), "\n")
   if(!is.null(x$Forecasts_Test)){
-    cat("For forecasts (test set): ", paste0(deparse(substitute(x)), "$Forecasts_Test"), "\n")
+    cat("For forecasts (test set): ", paste0(x$data, "$Forecasts_Test"), "\n")
   }
-  cat("See ", paste0("str(", deparse(substitute(x)), ")"), " for full list.")
+  cat("See ", paste0("str(", x$data, ")"), " for full list.")
   
   if(plot==TRUE){
-    plot.foreccomb_res_summary(x)
+    print(x$plot)
   }
 }
 
